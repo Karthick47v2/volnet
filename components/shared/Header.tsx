a715afcafa48cf926eabe3943663993086be955e
Karthick T. Sharma <63592766+Karthick47v2@users.noreply.github.com>
2024-02-01 08:12:22 +0530

    update ui

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -9,11 +9,12 @@ const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
-        <Link href="/" className="w-36">
+        <Link href="/" className="flex items-center">
           <Image 
             src="/assets/images/volunet.svg" width={40} height={38}
             alt="VoluNet logo" 
           />
+          <span className="ml-2 text-3xl font-bold bg-gradient-to-r from-red-500 via-yellow-500 to-purple-600 text-transparent bg-clip-text font-mono tracking-wider subpixel-antialiased">VoluNet</span>
         </Link>
 
         <SignedIn>
